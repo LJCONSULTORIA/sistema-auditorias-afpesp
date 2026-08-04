26fbb9b8b472a7b1558830e1f5cb1d2ee602e1d2
LLJAFPESP <lejunior@afpesp.org.br>
2026-08-04 14:46:12 -0300

    Atualiza paleta visual para azul-marinho

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default {content:['./index.html','./src/**/*.{js,ts,jsx,tsx}'],theme:{extend:{colors:{afpesp:{50:'#eef7f4',100:'#d6ebe4',500:'#147a64',600:'#0f6654',700:'#0d5144',900:'#123b34'}}}},plugins:[]};
+export default {content:['./index.html','./src/**/*.{js,ts,jsx,tsx}'],theme:{extend:{colors:{afpesp:{50:'#eef4ff',100:'#dbe8ff',300:'#8eb1e8',500:'#245a9b',600:'#173f73',700:'#102f59',800:'#0b2447',900:'#071a35'}}}},plugins:[]};
